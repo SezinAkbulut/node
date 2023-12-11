a0dfac5baaa9f922559f8fda908bef4b2c49d1f5
sezin akbulut <sezin.akbulut@gmail.com>
2023-12-11 12:12:06 +0100

    creat a repo

---
+++ server.js
@@ -0,0 +1,43 @@
+console.log("Hello Wold");
+console.log("This is a console log from my server.js file");
+
+const path = require("path");
+// Concatenate paths
+console.log(path.join(__dirname, "test", "server.js", "utf-8"));
+
+const fs = require("fs");
+
+// Read file
+/*try {
+  const fileContent = fs.readFileSync(
+    path.join(__dirname, "server.js", "utf-8")
+  );
+
+  console.log(fileContent);
+} catch (err) {
+  console.error(err.code);
+  console.error(err.message);
+}
+*/
+
+try {
+  const filePath = path.join(__dirname, "client", "index.html");
+
+  fs.writeFileSync(
+    filePath,
+    "<!DOCTYPE html><html lang'en'><head><meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'> <link rel='stylesheet' href='./style.css'><title>Document</title> </head><body><h1></h1></body></html>"
+  );
+  //fs.appendFileSync(filePath, "<h1></h1>");
+} catch (err) {
+  console.error(err.code);
+  console.error(err.message);
+}
+
+try {
+  const filePath = path.join(__dirname, "client", "style.css");
+
+  fs.writeFileSync(filePath, "body {background-color: blue;}");
+} catch (err) {
+  console.error(err.code);
+  console.error(err.message);
+}
