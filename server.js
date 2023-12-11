d82855c46456d2509266e6b61f7ea929f87e86fb
sezin akbulut <sezin.akbulut@gmail.com>
2023-12-11 12:33:00 +0100

    add index.html and style.css in about folder

--- server.js
+++ server.js
@@ -81,3 +81,26 @@ try {
   console.error("Error code :", err.code);
   console.error(err.message);
 }
+
+// index.html files within about folder
+try {
+  const filePath = path.join(__dirname, "./client/about", "index.html");
+
+  fs.writeFileSync(
+    filePath,
+    "<!DOCTYPE html><html lang'en'><head><meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'> <link rel='stylesheet' href='./style.css'><title>Document</title> </head><body><h1></h1></body></html>"
+  );
+} catch (err) {
+  console.error(err.code);
+  console.error(err.message);
+}
+
+//about folder within style.css
+try {
+  const filePath = path.join(__dirname, "./client/about", "style.css");
+
+  fs.writeFileSync(filePath, "body {background-color: red;}");
+} catch (err) {
+  console.error(err.code);
+  console.error(err.message);
+}
