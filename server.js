a36f2d7cd9744c928178219cfa7945706730b3ad
sezin akbulut <sezin.akbulut@gmail.com>
2023-12-11 12:28:48 +0100

    add a index.html file with content in contact folder

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ console.log(path.join(__dirname, "test", "server.js", "utf-8"));
 const fs = require("fs");
 
 // Read file
-/*try {
+try {
   const fileContent = fs.readFileSync(
     path.join(__dirname, "server.js", "utf-8")
   );
@@ -18,7 +18,6 @@ const fs = require("fs");
   console.error(err.code);
   console.error(err.message);
 }
-*/
 
 //client folder within index.html
 try {
@@ -49,5 +48,18 @@ try {
   fs.mkdirSync(path.join(__dirname, "./client/contact"));
 } catch (err) {
   console.error("Error code :", err.code);
+  console.error(err.message);
+}
+
+// index.html files within contact folder
+try {
+  const filePath = path.join(__dirname, "./client/contact", "index.html");
+
+  fs.writeFileSync(
+    filePath,
+    "<!DOCTYPE html><html lang'en'><head><meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'> <link rel='stylesheet' href='./style.css'><title>Document</title> </head><body><h1></h1></body></html>"
+  );
+} catch (err) {
+  console.error(err.code);
   console.error(err.message);
 }
